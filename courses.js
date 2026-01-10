31c08548aef807ea659f69a4ace1806ad78fc833
HeartBeat405 <142191809+HeartBeat405@users.noreply.github.com>
2026-01-10 15:31:12 +0700

    Add files via upload

---
+++ courses.js
@@ -0,0 +1,21 @@
+const courses = [
+    {
+      title: "Scratch Programming",
+      desc: "Belajar logika coding lewat game dan animasi.",
+      image: "ai.jpg",
+      link: "scratch.html"
+    },
+    {
+      title: "Web Development",
+      desc: "HTML, CSS, dan JavaScript untuk pemula.",
+      image: "ai.jpg",
+      link: "web.html"
+    },
+    {
+      title: "Game Development",
+      desc: "Membuat game seru sambil belajar coding.",
+      image: "ai.jpg",
+      link: "game.html"
+    }
+  ];
+
